5700e69229d164bd397f2d0226c4182cd606fcdd
Duncan McGreggor <oubiwann@gmail.com>
2018-04-30 10:58:25 -0500

    Kafka convenience functions for dev.

--- dev-resources/src/hxgm30/event/dev.clj
+++ dev-resources/src/hxgm30/event/dev.clj
@@ -1,5 +1,36 @@
 (ns hxgm30.event.dev
   (:require
     [clojure.core.async :as async]
-    [kinsky.client :as client]
-    [kinsky.async :as async]))
+    [clojure.tools.namespace.repl :refer [refresh]]
+    [kinsky.async :as kafka]
+    [kinsky.client :as kafka-client]))
+
+(def kafka-server {:bootstrap.servers "localhost:9092"})
+
+(defn send-msgs
+  []
+  (let [ch (kafka/producer kafka-server :keyword :edn)]
+    (async/go
+      (async/>! ch {:topic "account" :key :a :value {:data 1}})
+      (async/>! ch {:topic "account" :key :b :value {:data 2}}))))
+
+(defn rcv-msgs
+  []
+  (let [[rec-ch ctl-ch] (kafka/consumer
+                         (assoc kafka-server
+                                :group.id (str (java.util.UUID/randomUUID)))
+                         :keyword
+                         :edn)
+        topic "account"]
+    (async/put! ctl-ch {:op :subscribe :topic topic})
+    (async/go-loop []
+      (when-let [record (async/<! rec-ch)]
+        (println (pr-str record))
+        (recur)))
+    (send-msgs)))
+
+
+    ; (async/go-loop []
+    ;   (when-let [record (async/<! rec-ch)]
+    ;     (println (pr-str record))
+    ;     (recur)))
